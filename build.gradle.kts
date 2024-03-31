490dc8a0b2e7da2df1323c745ac35569e88ad63f
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-04-01 01:14:30 +0300

    update some dependencies libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies {
-        val hiltVersion by extra("2.51")
+        val hiltVersion by extra("2.51.1")
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
     }
 }
