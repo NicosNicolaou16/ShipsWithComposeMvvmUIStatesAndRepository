f1525da14597df195997cc5a9b7cbfee90650603
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-06-24 00:20:05 +0300

    update Gradle, update README file and update all libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
 }
 plugins {
-    id("com.android.application") version "8.4.1" apply false
+    id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
 }
