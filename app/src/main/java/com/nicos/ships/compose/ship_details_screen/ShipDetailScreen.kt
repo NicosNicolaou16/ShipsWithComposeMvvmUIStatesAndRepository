984413fc4ae462ed58ac59bb8caf63e36f904ffa
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-07-28 01:20:38 +0300

    redesign ship details screen

--- app/src/main/java/com/nicos/ships/compose/ship_details_screen/ShipDetailScreen.kt
+++ app/src/main/java/com/nicos/ships/compose/ship_details_screen/ShipDetailScreen.kt
@@ -1,10 +1,21 @@
 package com.nicos.ships.compose.ship_details_screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.rememberScaffoldState
@@ -13,12 +24,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -84,8 +98,48 @@ private fun ShipDetailsView(
                     .fillMaxWidth()
                     .height(height = 300.dp)
             )
-            Text(
-                text = shipData?.ship_name ?: "",
+            BasicInfo(shipData)
+        }
+    }
+}
+
+@Composable
+private fun BasicInfo(shipData: ShipsModel?) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(9.dp)
+    ) {
+        Column(horizontalAlignment = Alignment.Start) {
+            androidx.compose.material.Text(
+                text = stringResource(R.string.ship_name) + shipData?.ship_name,
+                modifier = Modifier.width(width = 300.dp),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                style = TextStyle(
+                    fontSize = 21.sp,
+                    textAlign = TextAlign.Start,
+                    fontWeight = FontWeight.Bold,
+                ),
+                color = Color.White,
+            )
+            androidx.compose.material.Text(
+                text = stringResource(R.string.ship_type) + shipData?.ship_type,
+                style = TextStyle(
+                    fontSize = 21.sp,
+                    textAlign = TextAlign.Start,
+                    fontWeight = FontWeight.Bold
+                ),
+                color = Color.White,
+            )
+        }
+        Column(
+            verticalArrangement = Arrangement.spacedBy(3.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            androidx.compose.material.Text(
+                text = stringResource(R.string.is_active),
                 style = TextStyle(
                     fontSize = 21.sp,
                     textAlign = TextAlign.Center,
@@ -93,14 +147,11 @@ private fun ShipDetailsView(
                 ),
                 color = Color.White,
             )
-            Text(
-                text = shipData?.ship_type ?: "",
-                style = TextStyle(
-                    fontSize = 21.sp,
-                    textAlign = TextAlign.Center,
-                    fontWeight = FontWeight.Bold
-                ),
-                color = Color.White,
+            Box(
+                modifier = Modifier
+                    .size(30.dp)
+                    .clip(shape = CircleShape)
+                    .background(if (shipData?.active == true) Color.Green else Color.Red)
             )
         }
     }
