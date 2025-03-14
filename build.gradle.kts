b682c20d079e26e5544dc6dff8d23c33937613ff
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-03-14 21:49:59 +0200

    update KSP

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,5 +8,5 @@ buildscript {
 plugins {
     id("com.android.application") version "8.9.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.10" apply false
-    id("com.google.devtools.ksp") version "2.1.10-1.0.29" apply false
+    id("com.google.devtools.ksp") version "2.1.10-1.0.31" apply false
 }
