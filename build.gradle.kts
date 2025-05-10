83ab5ffcbde2efe351a4443f8f49019d8a9b6374
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-05-11 01:24:40 +0300

    update Kotlin and update KSP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies {
-        val hiltVersion by extra("2.54")
+        val hiltVersion by extra("2.56.2")
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
     }
 }
 plugins {
     id("com.android.application") version "8.10.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
-    id("com.google.devtools.ksp") version "2.1.10-1.0.31" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+    id("com.google.devtools.ksp") version "2.1.20-2.0.1" apply false
 }
