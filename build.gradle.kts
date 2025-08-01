ccc42818e5416acc232f7fe878f459eeabfa4d68
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-08-01 20:18:03 +0300

    update Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
 }
 plugins {
-    id("com.android.application") version "8.10.0" apply false
+    id("com.android.application") version "8.12.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.20" apply false
     id("com.google.devtools.ksp") version "2.1.20-2.0.1" apply false
 }
