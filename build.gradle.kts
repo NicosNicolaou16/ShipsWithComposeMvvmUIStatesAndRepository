875fdd8c727e3d04804e485c7b1ab3beebbcafd2
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-02-14 01:57:28 +0200

    update KSP version to 2.3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,5 +8,5 @@ buildscript {
 plugins {
     id("com.android.application") version "9.0.1" apply false
     id("org.jetbrains.kotlin.android") version "2.3.10" apply false
-    id("com.google.devtools.ksp") version "2.3.4" apply false
+    id("com.google.devtools.ksp") version "2.3.5" apply false
 }
