8ece7d970125689cde68ed9b434a2db7b97fc6d1
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-02-14 01:56:51 +0200

    update Koltin version to 2.3.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,6 @@ buildscript {
 }
 plugins {
     id("com.android.application") version "9.0.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
     id("com.google.devtools.ksp") version "2.3.4" apply false
 }
